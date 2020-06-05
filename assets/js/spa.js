b117069c09181bcbe7eedf832e6ef404bbbb9db2
arch <z@wqc.me>
2020-06-05 16:22:42 +0300

    Small fix with local storage

--- assets/js/spa.js
+++ assets/js/spa.js
@@ -3,7 +3,7 @@ var theData = {};
 function formatBytes(a,b=2){if(0===a)return"0 Bytes";const c=0>b?0:b,d=Math.floor(Math.log(a)/Math.log(1024));return parseFloat((a/Math.pow(1024,d)).toFixed(c))+" "+["Bytes","KB","MB","GB","TB","PB","EB","ZB","YB"][d]}
 
 function colorchange() {
-    if (theData._coord._color === null &&
+    if (theData._coord._data._color === null &&
         !document.getElementById('scales').checked) return;
 
     theData._coord.options.draw.mode =
@@ -12,7 +12,7 @@ function colorchange() {
             "print";
 
     theData._coord.options.skip.dims.mode = "show";
-    theData._coord.options.skip.dims.values = theData._coord._graph_features;
+    theData._coord.options.skip.dims.values = theData._coord._data._graph_features;
 
     theData._coord.updateData("ParallelCoordinatesGraph",
         theData._currentData.features,
@@ -64,7 +64,33 @@ function handleFileSelect(evt) {
                     objects: results.data.filter((x, i) => i > 0)
                 };
 
-                localStorage.setItem('latest_data', JSON.stringify(data));
+                let latest = [JSON.stringify(data)];
+
+                for(let i = 0; i < 5; i++)
+                    if(localStorage.getItem('latest_data' + i) !== null) {
+                        latest.push(localStorage.getItem('latest_data' + i));
+                        localStorage.removeItem('latest_data' + i);
+                    }
+
+                latest.filter((x, i) => {
+                    if (i === 0) return true;
+                    let obj = JSON.parse(x);
+
+                    return !(data.features.every((y, j) => y === obj.features[j]) &&
+                        data.objects.length === obj.objects.length &&
+                        data.objects.every((y, j) =>
+                            y.every((z, l) => z === obj.objects[j][l])));
+                }).forEach((x, i) => {
+                    try {
+                        if (i < 5) localStorage.setItem('latest_data' + i, x);
+                    }
+                    catch (e) {
+                        let obj = JSON.parse(x);
+                        console.log('Internal browser storage size exceeded, ' +
+                            'skipping file "' + obj.file.name + '" save.');
+                    }
+                });
+
                 loadData(data);
             }
         });
